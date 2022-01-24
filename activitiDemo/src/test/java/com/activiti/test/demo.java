80a16d6faff4087aed2c305538b87f93a6c59f32
xujiale <1033324990@qq.com>
2022-01-24 09:55:14 +0800

    actibpm插件学习

--- activitiDemo/src/test/java/com/activiti/test/demo.java
+++ activitiDemo/src/test/java/com/activiti/test/demo.java
@@ -1,6 +1,7 @@
 package com.activiti.test;
 
 import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.ProcessEngines;
 import org.junit.Test;
 
@@ -18,5 +19,19 @@ public class demo {
         System.out.println(engine);
 
     }
+
+
+//    /**
+//     * 没有按照默认的方式加载classpath下的 activiti.cfg.xml文件
+//     * 自定义方式加载配置文件
+//     */
+//    @Test
+//    public void test02(){
+//        //首先创建ProcessEngineConfiguration对象
+//        ProcessEngineConfiguration configuration =
+//                ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration("activiti.cfg.xml");
+//        //通过ProcessEngineConfiguration对象来创建 ProcessEngine对象
+//        ProcessEngine processEngine = configuration.buildProcessEngine();
+//    }
 
 }
